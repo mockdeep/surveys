7305388b34c9179464abef3f4874f428e3b4e701
Robert Fletcher <lobatifricha@gmail.com>
2012-12-16 19:03:53 -0800

    initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveys::Application.config.secret_token = 'ca71b5abc73086d375aec6a64a4c0c55dc80b2b3b6f86050a687701cc4d92ce7ddd2e1b568ac75d022f33ca6c0696883ac34eac6295ef15769f8893722c8a2e2'
