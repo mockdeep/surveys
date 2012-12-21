6b53253ac7ff1ac9ed2f803902ec53f9dd2a5f74
David Ladowitz <david@ladowitz.com>
2012-12-21 00:51:05 -0800

    added authentication, user model, user controller and session controller

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
+
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 6, :message => "- yo dawg, make it longer"  }
+  validates :password_confirmation, :presence => true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Surveys::Application.routes.draw do
+  root to: 'users#new'
+
+  resources :users
+  resources :sessions
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
