e8e0d1151d3a6e527ef6a11bba97ce1887b094f1
David Ladowitz <david@ladowitz.com>
2012-12-30 17:44:59 -0800

    added skip_filters to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
 
-  helper_method :current_user
+    helper_method :current_user
 
-  def authenticate_user!
-    redirect_to signin_path unless current_user
-  end
+    def authenticate_user!
+      redirect_to signin_path unless current_user
+    end
 end
